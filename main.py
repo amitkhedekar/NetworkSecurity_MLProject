b3ce7ee6151f1421ad28e8c3861b470edbefa376
Khedekar <amit.khedekar@ingrammicro.com>
2025-09-20 13:17:17 +0530

    Data Validation added

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import sys
 from src.components.data_injestion import DataIngestion
+from src.components.data_validation import DataValidation
 from src.exception.exception import NetworkSecurityException
 from src.logging.logger import logging
-from src.entity.config_entity import DataIngestionConfig
+from src.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from src.entity.config_entity import TrainingPipelineConfig
 
 if __name__ == "__main__":
     try:
-        trainingpipelineconfig = TrainingPipelineConfig()
-        data_ingestion_config = DataIngestionConfig(training_pipeline_config = trainingpipelineconfig)
+        training_pipeline_config = TrainingPipelineConfig()
+
+        ##Data Ingestion steps
+        data_ingestion_config = DataIngestionConfig(training_pipeline_config = training_pipeline_config)
         data_ingestion = DataIngestion(data_ingestion_config = data_ingestion_config)
         logging.info("Initiate the data ingestion")
-
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation completed")
         print(data_ingestion_artifact)
+        
+        ##Data Validation steps
+        data_validation_config = DataValidationConfig(training_pipeline_config = training_pipeline_config)
+        data_validation = DataValidation( data_ingestion_artifact = data_ingestion_artifact,
+                                          data_validation_config = data_validation_config)
+        logging.info("Initiate Data Validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data Validation completed")
+        print(data_validation_artifact)
+        
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -5,7 +5,7 @@ import os
 
 from src.constants import training_pipeline
 
-class TrainingPipelineConfig:
+class TrainingPipelineConfig:  ##parameter/constants/data needed for Training stage
     def __init__(self, timestamp=datetime.now()):
         timestamp = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
         self.pipeline_name = training_pipeline.PIPELINE_NAME
@@ -13,8 +13,9 @@ class TrainingPipelineConfig:
         self.artifact_dir = os.path.join(self.artifact_name,timestamp)
         self.timestamp : str = timestamp
 
-class DataIngestionConfig:
+class DataIngestionConfig: ##parameter/constants/data needed for Ingestion stage
     def __init__(self, training_pipeline_config:TrainingPipelineConfig):
+
         
         self.data_ingestion_dir :str = os.path.join(
                                                      training_pipeline_config.artifact_dir,
@@ -42,3 +43,31 @@ class DataIngestionConfig:
         self.train_test_split_ratio :float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
         self.collection_name :str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
         self.database_name :str =training_pipeline.DATA_INGESTION_DATABASE_NAME
+
+class DataValidationConfig: ##parameter/constants/data needed for Validation stage
+    def __init__(self, training_pipeline_config:TrainingPipelineConfig):
+        self.data_validation_dir :str = os.path.join( training_pipeline_config.artifact_dir,
+                                                      training_pipeline.DATA_VALIDATION_DIR_NAME )
+        
+        self.valid_data_dir :str = os.path.join( self.data_validation_dir ,
+                                                 training_pipeline.DATA_VALIDATION_VALID_DIR )
+        
+        self.invalid_data_dir :str = os.path.join( self.data_validation_dir ,
+                                                   training_pipeline.DATA_VALIDATION_INVALID_DIR )
+        
+        self.valid_train_file_path :str = os.path.join( self.valid_data_dir ,
+                                                        training_pipeline.TRAIN_FILE_NAME )
+
+        self.valid_test_file_path :str = os.path.join( self.valid_data_dir ,
+                                                       training_pipeline.TEST_FILE_NAME )
+
+        self.invalid_train_file_path :str = os.path.join( self.invalid_data_dir ,
+                                                          training_pipeline.TRAIN_FILE_NAME )
+
+        self.invalid_test_file_path :str = os.path.join( self.invalid_data_dir ,
+                                                         training_pipeline.TEST_FILE_NAME )
+
+        self.drift_report_file_path :str = os.path.join( self.data_validation_dir ,
+                                                         training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
+                                                         training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME
+                                                       )
